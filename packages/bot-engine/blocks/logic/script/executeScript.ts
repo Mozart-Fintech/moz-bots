a3777e10ccf041d1c35244663dc3415496c7fc12
Clyzer <clyzer.dev@gmail.com>
2024-08-15 17:00:09 -0300

    Force serverside scripts in whatsapp

--- packages/bot-engine/blocks/logic/script/executeScript.ts
+++ packages/bot-engine/blocks/logic/script/executeScript.ts
@@ -18,7 +18,7 @@ export const executeScript = async (
   const isExecutedOnClient =
     block.options.isExecutedOnClient ?? defaultScriptOptions.isExecutedOnClient
 
-  if (!isExecutedOnClient) {
+  if (!isExecutedOnClient || state.whatsApp) {
     const { newVariables, error } = await executeFunction({
       variables,
       body: block.options.content,
